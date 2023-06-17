9cc67e00050e3b4cf2bb4257b911e3c16a997246
uralaltan <112475938+uralaltan@users.noreply.github.com>
2023-06-17 18:16:24 +0300

    Updated api

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-const { chiSquareGoodnessOfFit } = require("./distributions");
+// Chi Square Test
+const ChiSquare = require('./chiSquareTest.js');
+
+// Distributions
+const NormalDistribution = require('./distributions/normalDistribution.js');
+const BinomialDistribution = require('./distributions/binomialDistribution.js');
+const PoissonDistribution = require('./distributions/poissonDistribution.js');
+const ExponentialDistribution = require('./distributions/exponentialDistribution.js');
+const GammaDistribution = require('./distributions/gammaDistribution.js');
+const LogDistribution = require('./distributions/logDistribution.js');
+const LogNormalDistribution = require('./distributions/logNormalDistribution.js');
+const BetaDistribution = require('./distributions/betaDistribution.js');
+const NegativeBinomialDistribution = require('./distributions/negativeBinomialDistribution.js');
+// const DirichletDistribution = require('./distributions/dirichletDistribution.js');
+
+// Distribution Classes
+const normalDistribution = new NormalDistribution();
+const binomialDistribution = new BinomialDistribution(numTrials=100, successProbability=0.5);
+const poissonDistribution = new PoissonDistribution();
+const exponentialDistribution = new ExponentialDistribution();
+const gammaDistribution = new GammaDistribution();
+const logDistribution = new LogDistribution();
+const logNormalDistribution = new LogNormalDistribution();
+const betaDistribution = new BetaDistribution();
+const negativeBinomialDistribution = new NegativeBinomialDistribution(successesRequired=5, probabilityOfSuccess=0.5);
+// const dirichletDistribution = new DirichletDistribution();
 
 const express = require('express');
 const app = express();
@@ -22,9 +47,19 @@ app.post('/evaluate', (req, res) => {
     return;
   }
 
-  const inputList = userData.data; 
+  const testData = userData.data; 
 
-  const result = chiSquareGoodnessOfFit(inputList);
+  const result = new ChiSquare(testData, {
+    "Normal Distribution": normalDistribution.convertToNormalDistribution(testData),
+    "Binomial Distribution": binomialDistribution.convertToBinomialDistribution(testData),
+    "Poisson Distribution": poissonDistribution.convertToPoissonDistribution(testData),
+    "Exponential Distribution": exponentialDistribution.convertToExponentialDistribution(testData),
+    "Gamma Distribution": gammaDistribution.convertToGammaDistribution(testData),
+    "Log Distribution": logDistribution.convertToLogDistribution(testData),
+    "Log Normal Distribution": logNormalDistribution.convertToLogNormalDistribution(testData),
+    "Beta Distribution": betaDistribution.convertToBetaDistribution(testData),
+    "Negative Binomial Distribution": negativeBinomialDistribution.convertToNegativeBinomialDistribution(testData)
+  }).calculate();
 
   res.json({ result: result });
 
