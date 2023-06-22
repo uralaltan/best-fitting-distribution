70a7357b97bcac33370966073311a03c9fc9a3a5
baranbingol1 <111634390+baranbingol1@users.noreply.github.com>
2023-06-22 22:16:43 +0300

    Update Filter.js

--- best-fitting-oop-demo/Filter.js
+++ best-fitting-oop-demo/Filter.js
@@ -1,59 +1,4 @@
-// This is a class for filtering the data. Right now it has only removeOutliers functionality. Additional features can be added.
-
-const { std } = require("mathjs");
-
 class Filter{
-
-  addMean(data) {
-    const newData = [...data]
-    const mean = newData.reduce((a, b) => a + b, 0) / newData.length;
-    for(let i = 0; i< newData.length; i++){
-      newData [i] +=mean;
-    }
-    return newData
-  }
-
-  subMean(data) {
-    const newData = [...data]
-      const mean = newData.reduce((a, b) => a + b, 0) / newData.length;
-      for(let i = 0; i< newData.length; i++){
-        newData [i] -=mean;
-      }
-      return newData;
-  }
-
-  multiplyMean(data) {
-    const newData = [...data]
-      const mean = newData.reduce((a, b) => a + b, 0) / newData.length;
-      for(let i = 0; i< newData.length; i++){
-        newData [i] *=mean;
-      }
-
-      return data;
-  }
-
-  divideMean(data) {
-    const newData = [...data]
-      const mean = newData.reduce((a, b) => a + b, 0) / newData.length;
-
-      for(let i = 0; i< newData.length; i++){
-        newData [i] /= mean;
-      }
-      return newData;
-  }
-
-  standarize(data) {
-    const newData = [...data]
-      const mean = newData.reduce((a, b) => a + b, 0) / newData.length;
-      const stdDev = Math.sqrt(newData.reduce((sum, value) => sum + Math.pow(value - mean, 2), 0) / newData.length);
-
-      for(let i = 0; i< newData.length; i++){
-        newData [i] = (newData [0] - mean) / stdDev;
-      }
-
-      return newData;
-  }
-
 
     removeOutliers(data) {
     // Sort the data in ascending order
@@ -72,10 +17,24 @@ class Filter{
     const lowerBound = q1 - 1.5 * iqr;
     const upperBound = q3 + 1.5 * iqr;
   
-    // Filter out the outliers
-    const filteredData = data.filter((value) => value >= lowerBound && value <= upperBound);
-  
-    return filteredData;
+    // Filter out the outliers and return
+    return data.filter((value) => value >= lowerBound && value <= upperBound);
+  }
+
+  shiftDistribution(data, shiftAmount){
+    return data.map((value) => value + shiftAmount);
+  }
+
+  scaleDistribution(data, scaleFactor){
+    return data.map((value) => value * scaleFactor);
+  }
+
+  standardizeDistribution(data){
+    const mean = data.reduce((sum, value) => sum + value, 0) / data.length;
+    const stdDev = Math.sqrt(
+      data.reduce((sum, value) => sum + Math.pow(value - mean, 2), 0) / data.length
+    );
+    return data.map((value) => (value - mean) / stdDev);
   }
 
 }  
