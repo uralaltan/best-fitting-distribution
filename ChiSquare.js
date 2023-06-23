7b87526145cd38a8b3936f62554a677d0f855408
baranbingol1 <111634390+baranbingol1@users.noreply.github.com>
2023-06-23 08:53:29 +0300

    Add files via upload

---
+++ ChiSquare.js
@@ -0,0 +1,17 @@
+const Math = require("mathjs");
+
+class ChiSquare {
+    calculateChiSquare(distribution, observed, returnExpected=false) {
+      var expected = distribution.convert(observed);
+      var chiSquare = 0;
+        for (let i = 0; i < observed.length; i++) {
+            chiSquare += Math.pow(observed[i] - expected[i], 2) / expected[i];
+        }
+        if (returnExpected) {
+          return expected, chiSquare;
+        }
+        return chiSquare;
+    }
+  }
+
+module.exports = ChiSquare;
