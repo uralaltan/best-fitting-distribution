a74f6327f9c242e111b575362c7e872e3bea2567
baranbingol1 <111634390+baranbingol1@users.noreply.github.com>
2023-06-23 08:53:10 +0300

    Add files via upload

---
+++ Distribution.js
@@ -0,0 +1,13 @@
+class Distribution {
+
+    convert(data) {
+        throw new Error('Method convert() must be implemented.');
+      }
+    
+    generateData() {
+        throw new Error('Method generateData() must be implemented.');
+      }    
+
+}
+
+module.exports = Distribution;
