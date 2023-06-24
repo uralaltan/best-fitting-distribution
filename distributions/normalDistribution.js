48753bda60905fe9e96ea0f4cfd7405f53b9ad5b
uralaltan <112475938+uralaltan@users.noreply.github.com>
2023-06-24 22:00:19 +0300

    Change convert method; pre-define mean, variance

--- distributions/normalDistribution.js
+++ distributions/normalDistribution.js
@@ -5,21 +5,24 @@ const Distribution = require('../distribution.js');
 class NormalDistribution extends Distribution {
     
     convert(data) {
+
         const mean = data.reduce((a, b) => a + b, 0) / data.length;
+
         const variance = data.reduce((a, b) => a + Math.pow(b - mean, 2), 0) / data.length;
         const stdDev = Math.sqrt(variance);
-    
-        const normalData = [];
-        for (let i = 0; i < data.length; i++) {
-            const ithNumber = Math.exp(Math.square((data[i] - mean) / stdDev) / (-2)) / (stdDev * Math.sqrt(2 * Math.pi));
-            normalData.push(ithNumber);
-        }
+
+        const normalData = data.map(num => (num - mean) / stdDev)
         return normalData;
     }
     
-    generateData(size, mean, variance) {
-        const distribution = gaussian(mean, variance);
+    generateData(size) {
+
+        const mean = 0;
+        const variance = 1;
+
+        const distribution = gaussian(mean, varience);
         const data = distribution.random(size);
+
         return data; 
     }
 
