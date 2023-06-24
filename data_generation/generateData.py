751d9b8405064d88a41b92ef30eee257de6c6263
uralaltan <112475938+uralaltan@users.noreply.github.com>
2023-06-24 21:23:24 +0300

    Add 3 new distribution

--- data_generation/generateData.py
+++ data_generation/generateData.py
@@ -1,36 +1,41 @@
 import numpy as np
-from scipy.stats import poisson, expon, gamma, lognorm, beta
+from scipy.stats import poisson, expon, gamma, lognorm, beta, logser
 
-# Set the random seed for reproducibility
 np.random.seed(42)
 
-# Generate data from the Normal distribution
+# Normal Distribution
 normal_data = list(np.random.normal(loc=0, scale=1, size=100).ravel())
 
-# Generate data from the Poisson distribution
+# Poisson Distribution
 poisson_data = list(poisson.rvs(mu=5, size=100).ravel())
 
-# Generate data from the Exponential distribution
+# Exponential Distribution
 exponential_data = list(expon.rvs(scale=1, size=100).ravel())
 
-# Generate data from the Gamma distribution
+# Gamma Distribution
 gamma_data = list(gamma.rvs(a=2, scale=2, size=100).ravel())
 
-# Generate data from the Lognormal distribution
-lognormal_data = list(lognorm.rvs(s=0.5, scale=1, size=100).ravel())
+# Lognormal Distribution
+log_normal_data = list(lognorm.rvs(s=0.5, scale=1, size=100).ravel())
 
-# Generate data from the Beta distribution
+# Beta Distribution
 beta_data = list(beta.rvs(a=2, b=5, size=100).ravel())
 
-print("Normal data is:")
-print(normal_data)
-print("poisson data is:")
-print(poisson_data)
-print("expo data is:")
-print(exponential_data)
-print("gamma data is:")
-print(gamma_data)
-print("lognormal data is:")
-print(lognormal_data)
-print("beta data is:")
-print(beta_data)
+# Log Distribution
+log_data = list(logser.rvs(p=0.5, size=100).ravel())
+
+# Binomial Distribution
+binomial_data = list(np.random.binomial(n=10, p=0.5, size=100).ravel())
+
+# Negative Binomial Distribution
+negative_binomial_data = list(np.random.negative_binomial(n=10, p=0.5, size=100).ravel())
+
+print("const normalTestData =", normal_data)
+print("const poissonTestData =", poisson_data)
+print("const exponentialTestData =", exponential_data)
+print("const gammaTestData =", gamma_data)
+print("const logNormalTestData =", log_normal_data)
+print("const betaTestData =", beta_data)
+print("const logTestData =", log_data)
+print("const binomialTestData =", binomial_data)
+print("const negativeBinomialTestData =", negative_binomial_data)
