dd7d881f0ec5a7233a3d3852308b1b9e1c309d5f
uralaltan <112475938+uralaltan@users.noreply.github.com>
2023-07-04 17:06:28 +0300

    update methods

--- Filter.js
+++ Filter.js
@@ -1,57 +1,39 @@
-const Math = require("mathjs");
+class Filter {
 
-class Filter{
-
-    removeOutliers(data) {
-    // Sort the data in ascending order
-    const sortedData = data.slice().sort((a, b) => a - b);
-  
-    const q1Index = Math.floor(sortedData.length * 0.25);
-    const q3Index = Math.floor(sortedData.length * 0.75);
+    scaleArray = (data) => {
+        const min = Math.min(...data);
+        const max = Math.max(...data);
         
-    const q1 = sortedData[q1Index];
-    const q3 = sortedData[q3Index];
+        const scaledData = data.map((value) => {
+            return ((value - min) / (max - min));
+        });
+        
+        return scaledData;
+    }
 
-    // Calculate the IQR
-    const iqr = q3 - q1;
-  
-    // Define the lower and upper bounds for outliers
-    const lowerBound = q1 - 1.5 * iqr;
-    const upperBound = q3 + 1.5 * iqr;
-  
-    // Filter out the outliers
-    const filteredData = data.filter((value) => value >= lowerBound && value <= upperBound);
-  
-    return filteredData;
-  }
+    convertToBins = (data, k) => {
+      
+        var binDataMean = [];
+        var binData = [];
+        
+        for (let i = 0; i < data.length / k; i++) {
 
-  minMax(data) {
-    var minValue = Math.min(...data);
-    var maxValue = Math.max(...data);
-  
-    var scaledData = data.map((value) => {
-      return (value - minValue) / (maxValue - minValue);
-    });
-  
-    return scaledData;
-  }
+            var tempList = [];
 
-  shiftDistribution(data, shiftAmount){
-    return data.map((value) => value + shiftAmount);
-  }
+            var sum = 0;
+            for (let j = 0; j < k; j++) {
+                tempList.push(data[j+ k*i])
+                sum += data[j+ k*i]
+            }
 
-  scaleDistribution(data, scaleFactor){
-    return data.map((value) => value * scaleFactor);
-  }
+            sum /= k
 
-  standardizeDistribution(data){
-    const mean = data.reduce((sum, value) => sum + value, 0) / data.length;
-    const stdDev = Math.sqrt(
-      data.reduce((sum, value) => sum + Math.pow(value - mean, 2), 0) / data.length
-    );
-    return data.map((value) => (value - mean) / stdDev);
-  }
+            binDataMean.push(Math.round(sum));
+            binData.push(tempList);
+        }
 
+        return binDataMean;
+    }
 }  
 
 module.exports = Filter;
